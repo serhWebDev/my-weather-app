e0915859544df917d47c657602ac3a47487820e2
Serhii Vekshyn <serhiivekshyn@gmail.com>
2019-02-03 00:42:55 +0200

    2 style

--- src/App.js
+++ src/App.js
@@ -1,29 +1,79 @@
 import React, { Component } from 'react';
 
+import APIkey from './config';
+
 import Titles from './components/Titles';
 import Form from './components/Form';
+import Weather from "./components/Weather";
+
 import './App.css';
 
-const API_KEY = '8ac30abc28e4020b28c829281ddf543e';
+const API_KEY = APIkey.APIkey;
 
 class App extends Component {
+    state = {
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: undefined
+    };
     getWeather = async (e) => {
         e.preventDefault();
         const city = e.target.city.value;
         const country = e.target.country.value;
-        console.log(country);
         const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`);
         const data = await api_call.json();
-        console.log(data);
+        if (city && country){
+            console.log(data);
+            this.setState({
+                temperature: data.main.temp,
+                city: data.name,
+                country: data.sys.country,
+                humidity: data.main.humidity,
+                description: data.weather[0].description,
+                error: ''
+            })
+        } else {
+            console.log(data);
+            this.setState({
+                temperature: undefined,
+                city: undefined,
+                country: undefined,
+                humidity: undefined,
+                description: undefined,
+                error: 'Please, enter the value'
+            })
+        }
     };
     render() {
         return (
-            <div className="App">
-                <Titles />
-                <Form getWeather={this.getWeather}/>
+            <div>
+                <div className="wrapper">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-xs-5 title-container">
+                                <Titles />
+                            </div>
+                            <div className="col-xs-7 form-container">
+                                <Form getWeather={this.getWeather}/>
+                                <Weather
+                                    temperature={this.state.temperature}
+                                    city={this.state.city}
+                                    country={this.state.country}
+                                    humidity={this.state.humidity}
+                                    description={this.state.description}
+                                    error={this.state.error}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
 }
+
 
 export default App;
--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const API_KEY = '8ac30abc28e4020b28c829281ddf543e';
-
-export default API_KEY;
+export default {
+    APIkey: '8ac30abc28e4020b28c829281ddf543e'
+};
